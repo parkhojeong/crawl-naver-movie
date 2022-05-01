e44b265c7e00ce5613b73c02cdcff5b1497719af
parkhojeong <parkhj062@gmail.com>
2022-05-01 23:00:37 +0900

    add crawl movie data

---
+++ crawl_movie_data.py
@@ -0,0 +1,38 @@
+import time
+
+import requests
+import bs4
+import csv
+
+URL = "https://movie.naver.com/movie/point/af/list.naver?&page=1"
+site_text = requests.get(URL, {}).text
+
+# get html
+web_page = bs4.BeautifulSoup(site_text, "lxml")
+
+# parse
+title_elements = web_page.find_all("td", {"class": "title"})
+
+START_PAGE = 1
+END_PAGE = 110
+movie_data = []
+for page in range(START_PAGE, END_PAGE):
+    site_text = requests.get("https://movie.naver.com/movie/point/af/list.naver?&page={}".format(page), {}).text
+    web_page = bs4.BeautifulSoup(site_text, "lxml")
+    title_elements = web_page.find_all("td", {"class": "title"})
+
+    for title in title_elements:
+        movie_name = title.find("a").contents
+        movie_score = title.find("em").contents
+        movie_sentense = title.find("a", {"class": "report"}).get("onclick").split("', '")[2]
+
+        movie_data.append({"movie": movie_name[0],  "sentense": movie_sentense, "score": movie_score[0]})
+
+    time.sleep(0.5)
+
+with open( "samples.csv", "w", newline="" ,encoding = 'utf8' ) as f:
+    #  TODO: encode unicode
+    write = csv.writer(f)
+    write.writerow(movie_data)
+
+# ------------------------------------------------------------------------------------------------
