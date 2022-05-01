33c072d302d0e089cdb3efe82cc6de4cfd8f5b3a
parkhojeong <parkhj062@gmail.com>
2022-05-01 23:01:13 +0900

    add csv movie review filtering parser

---
+++ MovieReivew.py
@@ -0,0 +1,17 @@
+from RawMovieReview import RawMovieReview
+
+class MovieReivew(RawMovieReview):
+    def __init__(self, score_threadhold, file_name="samples.csv", ):
+        super().__init__(file_name)
+        self.score_threadhold = score_threadhold
+
+    def __toTuple(self):
+        sampleTupleList = super()._getSampleTupleList()[:]
+
+        filteredSampleTuleList = []
+        for sample in sampleTupleList:
+            [_, sentense, score] = sample
+            filteredSampleTuleList.append((sentense.strip(), int(score) >= self.score_threadhold, score))
+
+        print(filteredSampleTuleList)
+        return filteredSampleTuleList
