18b1c001735bfcc4bd9cbe6d4abd6007d539c81c
parkhojeong <parkhj062@gmail.com>
2022-05-01 23:11:51 +0900

    sentence 비어 있는 경우 저장 안하도록 처리

--- crawl_movie_data.py
+++ crawl_movie_data.py
@@ -13,8 +13,8 @@ web_page = bs4.BeautifulSoup(site_text, "lxml")
 # parse
 title_elements = web_page.find_all("td", {"class": "title"})
 
-START_PAGE = 1
-END_PAGE = 110
+START_PAGE = 2
+END_PAGE = 3
 movie_data = []
 for page in range(START_PAGE, END_PAGE):
     site_text = requests.get("https://movie.naver.com/movie/point/af/list.naver?&page={}".format(page), {}).text
@@ -25,6 +25,9 @@ for page in range(START_PAGE, END_PAGE):
         movie_name = title.find("a").contents
         movie_score = title.find("em").contents
         movie_sentense = title.find("a", {"class": "report"}).get("onclick").split("', '")[2]
+
+        if(movie_sentense == ""):
+            continue
 
         movie_data.append({"movie": movie_name[0],  "sentense": movie_sentense, "score": movie_score[0]})
 
